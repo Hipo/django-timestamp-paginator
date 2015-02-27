49e1165b28b890b8f3782c929e0d372cabc1f79d
Umut Bozkurt <umut@hipo.biz>
2015-02-27 14:03:17 +0200

    packaging

--- timestamp_paginator/__init__.py
+++ timestamp_paginator/__init__.py
@@ -1,1 +1,16 @@
+"""
 
+Timestamp Paginator for Django.
+
+Umut Bozkurt - @umutbozkurt
+MIT Licence
+
+"""
+
+
+from timestamp_paginator.paginator import TimestampPaginator, Page
+
+__all__ = [
+    'TimestampPaginator',
+    'Page'
+]
